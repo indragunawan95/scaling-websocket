18ac1c04a11de5a7124c12de1208fe4e852e6ebf
indra <i.pt.gd.indra.gunawan@gmail.com>
2024-06-02 14:00:02 +0800

    revamp react using chat app view

--- horizontal-scaling-redis-haproxy/websocket-client/src/App.js
+++ horizontal-scaling-redis-haproxy/websocket-client/src/App.js
@@ -1,15 +1,64 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
+  const [ws, setWs] = useState(null);
+  const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState('');
+
   useEffect(() => {
-    const ws = new WebSocket("ws://localhost:8080");
-    ws.onopen = () => ws.send("Hello! I'm client");
-    ws.onmessage = message => console.log(`Received: ${message.data}`);
+    const socket = new WebSocket("ws://localhost:8080");
+    socket.onopen = () => {
+      console.log('WebSocket connected');
+    };
+    socket.onmessage = (message) => {
+      setMessages(prevMessages => [...prevMessages, message.data]);
+    };
+    socket.onclose = () => {
+      console.log('WebSocket disconnected');
+    };
+    setWs(socket);
+
+    return () => {
+      socket.close();
+    };
   }, []);
+
+  const sendMessage = () => {
+    if (ws && input) {
+      ws.send(input);
+      setInput('');
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      sendMessage();
+    }
+  };
 
   return (
     <div className="App">
-      <h1>WebSocket Client</h1>
+      <h1>WebSocket Chat App</h1>
+      <div className="chat-box">
+        {messages.map((message, index) => (
+          <div key={index} className="message">{message}</div>
+        ))}
+      </div>
+      <div className="input-box">
+        <input
+          type="text"
+          value={input}
+          onChange={handleInputChange}
+          onKeyPress={handleKeyPress}
+          placeholder="Type a message..."
+        />
+        <button onClick={sendMessage}>Send</button>
+      </div>
     </div>
   );
 }
